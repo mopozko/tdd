982c8a74d6b89c39566856803f283bd2dde0148a
Andrey <mopozko123@gmail.com>
2017-11-03 21:28:47 +0500

    Новая реализация CircularCloudLayouter основанная на спирали Архимеда

--- TagsCloudVisualization/CircularCloudLayouter.cs
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -8,97 +9,56 @@ namespace TagsCloudVisualization
     public class CircularCloudLayouter
     {
         private Point center;
-        private bool[,] feeld;
-        private int width;
-        private int height;
         private List<Rectangle> cloudRectangles;
+        private ArchimedeanSpiral archimedeanSpiral;
         public CircularCloudLayouter(Point center)
         {
             this.center = center;
-            this.width = center.X * 2;
-            this.height = center.Y * 2;
-            feeld = new bool[width, height];
             cloudRectangles = new List<Rectangle>();
+            archimedeanSpiral = new ArchimedeanSpiral(center,0.1);
         }
-
-        private bool IsBorderPoint(int x, int y)
+        
+        public Rectangle PutNextRectangle(Size size)
         {
-            if (feeld[x, y]) return false;
-            for (var dx = -1; dx <= 1; dx++)
-                for (var dy = -1; dy <= 1; dy++)
-                    if (feeld[x + dx, y + dy]) return true;
-            return false;
-        }
-
-        public Rectangle PutNextRectangle(Size rectangleSize)
-        {
-            Rectangle result;
-            var w = rectangleSize.Width;
-            var h = rectangleSize.Height;
-            if (!cloudRectangles.Any())
-                result = new Rectangle(center.X - w / 2, center.Y - h / 2, w, h);
-            else
+            var w = size.Width;
+            var h = size.Height;
+            if (w <= 0 || h <= 0)
+                throw new ArgumentException();
+            foreach (var point in archimedeanSpiral)
             {
-                var potentialRectangles = new List<Rectangle>();
-
-                for (var x = w; x < width - w - 1; x++)
-                    for (var y = h; y < height - h - 1; y++)
-                        if (IsBorderPoint(x, y))
-                        {
-                            potentialRectangles.Add(new Rectangle(x, y, w, h));
-                            potentialRectangles.Add(new Rectangle(x - w + 1, y - h + 1, w, h));
-                            potentialRectangles.Add(new Rectangle(x - w + 1, y, w, h));
-                            potentialRectangles.Add(new Rectangle(x, y - h + 1, w, h));
-                        }
-
-                potentialRectangles = potentialRectangles
-                                      .Where(x => !cloudRectangles.Any(r => r.IntersectsWith(x)))
-                                      .ToList();
-
-                result = FindTheNearestArea(potentialRectangles);
+                var rect = new Rectangle(new Point(point.X - w / 2, point.Y - h / 2), size);
+                if (cloudRectangles.Any(x => x.IntersectsWith(rect))) continue;
+                cloudRectangles.Add(rect);
+                return rect;
             }
-            if (result != default(Rectangle))
-            {
-                cloudRectangles.Add(result);
-                UpdateFeeld(result);
-            }
-            return result;
+            return Rectangle.Empty;
         }
-
-        private Rectangle FindTheNearestArea(IEnumerable<Rectangle> potentialRectangles)
+        private class ArchimedeanSpiral : IEnumerable<Point>
         {
-            Rectangle result = default(Rectangle);
-            var minDistance = -1.0;
-            foreach (var rectangle in potentialRectangles)
+            private Point center;
+            private double removalRatio;
+            public ArchimedeanSpiral(Point center, double removalRatio)
             {
-                var distance = GetDistanceBetweenPoints(rectangle.GetCenter(), center);
-                if (minDistance == -1) minDistance = distance;
-                if (distance < minDistance)
+                this.center = center;
+                this.removalRatio = removalRatio;
+            }
+            public IEnumerator<Point> GetEnumerator()
+            {
+                var angleInDegrees = 0;
+                while (true)
                 {
-                    minDistance = distance;
-                    result = rectangle;
+                    var angleInRadians = angleInDegrees * Math.PI / 180;
+                    var radius = removalRatio * angleInRadians;
+                    yield return new Point(
+                        x: (int) (center.X + radius * Math.Cos(angleInRadians)),
+                        y: (int) (center.Y + radius * Math.Sin(angleInRadians)));
+                    angleInDegrees++;
                 }
             }
-            return result;
-        }
-        private void UpdateFeeld(Rectangle area)
-        {
-            for (var i = area.X; i < area.Width + area.X; i++)
-                for (var j = area.Y; j < area.Height + area.Y; j++)
-                    feeld[i, j] = true;
-        }
-
-        private double GetDistanceBetweenPoints(Point point1, Point point2)
-        {
-            return Math.Sqrt((point1.X - point2.X) * (point1.X - point2.X) +
-                             (point1.Y - point2.Y) * (point1.Y - point2.Y));
-        }
-    }
-    public static class MyExtensions
-    {
-        public static Point GetCenter(this Rectangle rect)
-        {
-            return new Point(rect.X + rect.Width / 2, rect.Y + rect.Height / 2);
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
         }
     }
 }
